eb3b11ebf86bdf8281cb394d80106e96c66e775b
Erich Purpur <epurpur@gmail.com>
2021-04-22 16:30:34 -0400

    adding scrollable table component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import MainMap from './components/MainMap';
 import TabularData from './components/TabularData';
 import streetlights from './streetlights.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
